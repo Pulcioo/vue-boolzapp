a04a9f44e5855839b5df5147465461f8f8ff35a9
Davide Pulci <davide.pulci@gmail.com>
2022-03-30 18:02:51 +0200

    fatto milestone 2

--- script.js
+++ script.js
@@ -9,6 +9,28 @@ Visualizzazione dinamica della lista contatti:
 tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
 */
 
+/*
+Milestone 2
+Visualizzazione dinamica dei messaggi: tramite la direttiva v-for,
+visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+Click sul contatto mostra la conversazione del contatto cliccato
+*/
+
+/*
+Milestone 3
+Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
+“enter” il testo viene aggiunto al thread sopra, come messaggio verde
+Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
+un “ok” come risposta, che apparirà dopo 1 secondo.
+*/
+
+/*
+Milestone 4
+Ricerca utenti: scrivendo qualcosa nell’input a sinistra,
+vengono visualizzati solo i contatti il cui nome contiene le lettere inserite 
+(es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+*/
+
 const contacts = [
     {
         name: 'Michele',
@@ -177,10 +199,14 @@ const app = new Vue(
     {
         el: '#vue-app',
         data: {
-            contacts
+            contacts,
+            activeContact: 0,
         },
         methods: {
-
+            setActiveContact(i) {
+                this.activeContact = i;
+                console.log(this.activeContact)
+            }
         }
     }
 );
